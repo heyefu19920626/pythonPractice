fbbf7d60cbf7a58966e260ba07f285ee4a0a3816
heyefu <527380349@qq.com>
2018-05-11 22:30:26 +0800

    type isinstance dir getattr hasattr setattr

--- objectOriented.py
+++ objectOriented.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import types
+
 # 括号内object表示继承的类，如果没有，则写object
 
 
@@ -51,3 +53,31 @@ class Dog(Animal):
 
 run(Dog())
 run(Student('Bob'))
+
+# type
+print(type(123))
+print(types.FunctionType == type(run))
+# isinstance
+print(isinstance(cat, Animal))
+print(isinstance(123, str))
+# dir
+print(dir('abc'))
+# attr
+print(hasattr(cat, 'run'))
+print(hasattr(cat, 'runs'))
+try:
+    print(cat.name)
+except Exception as e:
+    print(e)
+setattr(cat, 'name', 'Tome')
+print(cat.name)
+fn = getattr(cat, 'run')
+fn()
+
+
+def readImage(fp):
+    # 假设我们希望从文件流fp中读取图像，我们首先要判断该fp对象是否存在read方法，
+    # 如果存在，则该对象是一个流，如果不存在，则无法读取
+    if hasattr(fp, 'read'):
+        return readData(fp)
+    return None
